9a09e138f7ced5f16945aaa3edc4475f202cf91c
DaniloPia1 <danilo.favaron@escola.pr.gov.br>
2026-06-15 11:27:00 -0300

    asas

--- main.js
+++ main.js
@@ -3,18 +3,19 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function (){
-        for (let = 0; j <botoes.length; j++){
+        // Corrigido o 'let j = 0'
+        for (let j = 0; j < botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
-        botoes[j].classList.add("ativo");
-        textos[j].classList.add("ativo");
+        // Usando o índice 'i' do botão clicado para ativar
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
 
-
-
-const contadores = document.querySelectorAl(".contador");
+// Corrigido o querySelectorAll
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2028-01-01T00:00:00");
 const tempoObjetivo2 = new Date("2027-05-28T00:00:00");
 const tempoObjetivo3 = new Date("2027-02-23T00:00:00");
@@ -22,28 +23,8 @@ const tempoObjetivo4 = new Date("2028-08-12T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function calculaTempo(tempoObjetivo) {
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-    if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    } else {
-        return "Prazo Finalizado";
-    }
-}
-
 function atualizaCronometro(){
-
     for (let i = 0; i < tempos.length; i++){
-
         let tempoAtual = new Date();
         let tempoFinal = tempos[i] - tempoAtual;
 
@@ -72,7 +53,7 @@ function atualizaCronometro(){
 
 function comecaCronometro(){
     atualizaCronometro();
-    setInterval(atualizaCronometro,1000);
+    setInterval(atualizaCronometro, 1000);
 }
 
 comecaCronometro();
